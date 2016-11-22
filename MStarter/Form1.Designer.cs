acf8e8e072ac91f66bffea1847b5ca5c7bf78252
LeanZo <kashchannel@live.com>
2016-11-21 23:16:31 -0200

    Beta 1.0
    Funciona bem, falta apenas mais algumas opções e ser mais user friendly

--- MStarter/Form1.Designer.cs
+++ MStarter/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.botaoAdd = new System.Windows.Forms.Button();
+            this.comboBoxExe = new System.Windows.Forms.ComboBox();
+            this.botaoRemover = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // botaoAdd
+            // 
+            this.botaoAdd.Location = new System.Drawing.Point(12, 21);
+            this.botaoAdd.Name = "botaoAdd";
+            this.botaoAdd.Size = new System.Drawing.Size(75, 23);
+            this.botaoAdd.TabIndex = 0;
+            this.botaoAdd.Text = "Adicionar";
+            this.botaoAdd.UseVisualStyleBackColor = true;
+            this.botaoAdd.Click += new System.EventHandler(this.botaoAdd_Click);
+            // 
+            // comboBoxExe
+            // 
+            this.comboBoxExe.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxExe.FormattingEnabled = true;
+            this.comboBoxExe.Location = new System.Drawing.Point(12, 63);
+            this.comboBoxExe.Name = "comboBoxExe";
+            this.comboBoxExe.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxExe.TabIndex = 1;
+            // 
+            // botaoRemover
+            // 
+            this.botaoRemover.Location = new System.Drawing.Point(139, 61);
+            this.botaoRemover.Name = "botaoRemover";
+            this.botaoRemover.Size = new System.Drawing.Size(75, 23);
+            this.botaoRemover.TabIndex = 2;
+            this.botaoRemover.Text = "Remover";
+            this.botaoRemover.UseVisualStyleBackColor = true;
+            this.botaoRemover.Click += new System.EventHandler(this.botaoRemover_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(224, 99);
+            this.Controls.Add(this.botaoRemover);
+            this.Controls.Add(this.comboBoxExe);
+            this.Controls.Add(this.botaoAdd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Opacity = 0.97D;
+            this.ShowIcon = false;
+            this.Text = "MStarter";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button botaoAdd;
+        private System.Windows.Forms.ComboBox comboBoxExe;
+        private System.Windows.Forms.Button botaoRemover;
     }
 }
 
--- MStarter/Form1.cs
+++ MStarter/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
 
 namespace MStarter
 {
@@ -15,6 +18,149 @@ namespace MStarter
         public Form1()
         {
             InitializeComponent();
+            string FSdocPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarter.fsdoc");
+            string FSdocPathSafe = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarterSafe.fsdoc");
+
+            if (System.AppDomain.CurrentDomain.FriendlyName != "MStarter.exe")
+             {
+                 Run(FSdocPath);
+             }
+
+            string today = DateTime.Now.ToString("dd/MM/yyyy");
+            string month = DateTime.Now.Month.ToString();
+           // MessageBox.Show(today + "- BETA 1.0");
+
+
+            FileStream FSdoc;
+            FileStream FSdocSafe;
+            string limiteresp = "undefined";
+            bool limiteinterruptor = true;
+
+            if (File.Exists(FSdocPath))
+            {
+                FSdoc = new FileStream(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarter.fsdoc"), FileMode.Open, FileAccess.ReadWrite);
+                FSdocSafe = new FileStream(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarterSafe.fsdoc"), FileMode.Open, FileAccess.ReadWrite);
+                // MessageBox.Show("MStarter.fsdoc Encontrado");
+            }
+            else
+            {
+                MessageBox.Show("Arquivo 'MStarter.fsdoc' não foi encontrado\nCriando um novo arquivo 'MStarter.fsdoc'");
+                /*  Console.WriteLine("Qual o limite mensal do seu cartão (Deixe em branco caso não tenha limite)");
+                  Console.Write("R$: ");
+                  do
+                  {
+                      try
+                      {
+                          limiteresp = Console.ReadLine().Replace(',', '.');
+                          if (String.IsNullOrEmpty(limiteresp))
+                              limiteresp = "0";
+                          Convert.ToDouble(limiteresp);
+                          limiteinterruptor = false;
+                      }
+                      catch (Exception e)
+                      {
+                          Console.WriteLine("Você tentou inserir um valor invalido, insira apenas numeros");
+                          Console.Write("R$: ");
+                      }
+                  } while (limiteinterruptor); */
+                
+                    Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MStarter"));
+                using (File.Create(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarterSafe.fsdoc"))) { }
+                using (File.Create(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarter.fsdoc"))) { }
+                FSdoc = new FileStream(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarter.fsdoc"), FileMode.Open, FileAccess.ReadWrite);
+                FSdocSafe = new FileStream(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarterSafe.fsdoc"), FileMode.Open, FileAccess.ReadWrite);
+            }
+            FSdoc.Close();
+            FSdocSafe.Close();
+            /*  if (limiteresp != "undefined")
+                  File.AppendAllText(FSdocPath, limiteresp + Environment.NewLine); */
+            var textLines = File.ReadAllLines(FSdocPathSafe);
+            comboBoxExe.DataSource = textLines;
+            //MessageBox.Show("Feito!");
+
+            //Start(today, FSdocPath);
+        }
+
+        static void Run(string FSdocPath)
+        {
+            var textlines = File.ReadAllLines(FSdocPath);
+            foreach(var line in textlines)
+            {
+                System.Diagnostics.Process.Start(line);
+            }
+            Process[] processes = Process.GetProcessesByName(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+            foreach (var process in processes)
+            {
+                process.Kill();
+            }
+        } 
+
+        private void botaoAdd_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Title = "Selecione um programa ou arquivo";
+            openFileDialog1.InitialDirectory = "C:\\";
+
+            string FSdocPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarter.fsdoc");
+            string FSdocPathSafe = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarterSafe.fsdoc");
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+
+                string strfilename = openFileDialog1.FileName;
+                string strsafefilename = openFileDialog1.SafeFileName;
+                MessageBox.Show(strsafefilename);
+                File.AppendAllText(FSdocPath, strfilename + Environment.NewLine);
+                File.AppendAllText(FSdocPathSafe, strsafefilename + Environment.NewLine);
+
+                var textLines = File.ReadAllLines(FSdocPathSafe);
+                comboBoxExe.DataSource = textLines;
+
+            }
+        }
+
+        static void Remover(string FSdocPath, string[] textlines, int selected)
+        {
+            string[] dataArrayOrg = new string[textlines.Length - 1];
+            textlines[selected] = null;
+            int count = 0;
+            for (int i = 0; i < textlines.Length; i++)
+            {
+                if (textlines[i] != null)
+                {
+                    if (i == count)
+                    {
+                        string dataArraystring = textlines[i];
+                        dataArrayOrg[i] = dataArraystring;
+                        count++;
+                    }
+                    else
+                    {
+                        string dataArraystring = textlines[i];
+                        dataArrayOrg[i - 1] = dataArraystring;
+                        count++;
+                    }
+                }
+
+            }
+            File.WriteAllLines(FSdocPath, dataArrayOrg);
+        }
+
+        private void botaoRemover_Click(object sender, EventArgs e)
+        {
+            string FSdocPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarter.fsdoc");
+            string FSdocPathSafe = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"MStarter\MStarterSafe.fsdoc");
+            int selected = comboBoxExe.SelectedIndex;
+            var textlines = File.ReadAllLines(FSdocPath);
+            var textlinessafe = File.ReadAllLines(FSdocPathSafe);
+
+            Remover(FSdocPath, textlines, selected);
+            Remover(FSdocPathSafe, textlinessafe, selected);
+            textlinessafe = File.ReadAllLines(FSdocPathSafe);
+            comboBoxExe.DataSource = textlinessafe;
+
         }
     }
 }
